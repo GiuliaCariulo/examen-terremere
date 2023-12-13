a122d831b16af661cc6d37cb0caf7586866d473a
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2023-12-13 11:35:02 +0100

    menu mobile done

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ var toggleMenu = function toggleMenu() {
 };
 
 burger.addEventListener("click", toggleMenu);
+
+var carte = document.querySelector(".menu-mobile");
+var button = document.querySelector(".button-menu");
+
+var toggleMenu = function toggleMenu() {
+  carte.classList.toggle("is-active");
+};
+
+button.addEventListener("click", toggleMenu);
